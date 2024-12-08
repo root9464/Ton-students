42f60c39ef0449090ab21ce11b0158781d656ec2
io <senpai9464@yandex.ru>
2024-12-08 01:20:39 +0000

    init

--- backend/modules/validate_transaction/validate.nodule.go
+++ backend/modules/validate_transaction/validate.nodule.go
@@ -1,8 +1,8 @@
-package hello
+package validate_transaction
 
 import "github.com/gofiber/fiber/v2"
 
-func Hello(ctx *fiber.Ctx) error {
+func Validate(ctx *fiber.Ctx) error {
 	return ctx.JSON(fiber.Map{
 		"message": "hello world",
 	})
