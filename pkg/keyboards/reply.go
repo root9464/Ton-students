ca814bc0423e217940ffcc9fe02e56bd6200e195
dopamine <ivanbatutin921@mail.ru>
2024-12-19 07:48:55 +0500

    ddadsf

---
+++ pkg/keyboards/reply.go
@@ -0,0 +1,18 @@
+package keyboards
+
+// import (
+// 	"fmt"
+
+// 	"github.com/PaulSonOfLars/gotgbot/v2"
+// )
+
+// var replyMarkup = &gotgbot.InlineKeyboardMarkup{
+// 	InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+// 		{
+// 			{
+// 				Text:         "Ответить",
+// 				CallbackData: fmt.Sprintf("reply_%d", userID),
+// 			},
+// 		},
+// 	},
+// }
