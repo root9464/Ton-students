0e7a78942ad4e9191648ebea270af303436adabd
io <senpai9464@yandex.ru>
2024-12-21 17:03:13 +0000

    change a to app

--- backend/core/app/app.go
+++ backend/core/app/app.go
@@ -31,26 +31,26 @@ func NewApp() *App {
 	}
 }
 
-func (a *App) Run() error {
-	a.app.Use(cors.New(cors.Config{
+func (app *App) Run() error {
+	app.app.Use(cors.New(cors.Config{
 		AllowOrigins:     "http://localhost:5173, http://0.0.0.0:5173, https://4f67-95-105-125-55.ngrok-free.app",
 		AllowCredentials: true,
 	}))
-	a.app.Use(middleware.LoggerMiddleware())
+	app.app.Use(middleware.LoggerMiddleware())
 
-	a.initDeps()
+	app.initDeps()
 
-	return a.runHttpServer()
+	return app.runHttpServer()
 }
 
-func (a *App) initDeps() {
+func (app *App) initDeps() {
 	inits := []func() error{
-		a.initConfig,
-		a.initDb,
-		a.initLogger,
-		a.initValidator,
-		a.initModuleProvider,
-		a.initRouter,
+		app.initConfig,
+		app.initDb,
+		app.initLogger,
+		app.initValidator,
+		app.initModuleProvider,
+		app.initRouter,
 	}
 	for _, init := range inits {
 		err := init()
@@ -60,13 +60,13 @@ func (a *App) initDeps() {
 	}
 }
 
-func (a *App) initConfig() error {
-	if a.config == nil {
+func (app *App) initConfig() error {
+	if app.config == nil {
 		config, err := config.LoadConfig(".")
 		if err != nil {
 			return fmt.Errorf("✖ Failed to load config: %s", err.Error())
 		}
-		a.config = &config
+		app.config = &config
 	}
 
 	err := config.Load(".env")
@@ -77,13 +77,13 @@ func (a *App) initConfig() error {
 	return nil
 }
 
-func (a *App) initDb() error {
-	if a.db == nil {
-		db, err := ent.Open("postgres", a.config.DatabaseUrl)
+func (app *App) initDb() error {
+	if app.db == nil {
+		db, err := ent.Open("postgres", app.config.DatabaseUrl)
 		if err != nil {
 			return fmt.Errorf("✖ Failed to connect to database: %s", err.Error())
 		}
-		a.db = db
+		app.db = db
 
 		if err := db.Schema.Create(context.Background()); err != nil {
 			return fmt.Errorf("✖ Failed to create schema resources: %s", err.Error())
@@ -93,51 +93,51 @@ func (a *App) initDb() error {
 	return nil
 }
 
-func (a *App) initLogger() error {
-	if a.logger == nil {
-		a.logger = logger.GetLogger()
+func (app *App) initLogger() error {
+	if app.logger == nil {
+		app.logger = logger.GetLogger()
 	}
 	return nil
 }
 
-func (a *App) initValidator() error {
-	if a.validator == nil {
-		a.validator = validator.New()
+func (app *App) initValidator() error {
+	if app.validator == nil {
+		app.validator = validator.New()
 	}
 	return nil
 }
 
-func (a *App) initModuleProvider() error {
+func (app *App) initModuleProvider() error {
 	var err error
-	a.moduleProvider, err = NewModuleProvider(a)
+	app.moduleProvider, err = NewModuleProvider(app)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (a *App) runHttpServer() error {
-	if a.httpConfig == nil {
+func (app *App) runHttpServer() error {
+	if app.httpConfig == nil {
 		cfg, err := config.NewHTTPConfig()
 		if err != nil {
 			return fmt.Errorf("✖ Failed to load config: %s", err.Error())
 		}
-		a.httpConfig = cfg
+		app.httpConfig = cfg
 	}
 
-	log.Infof("🌐 Server is running on %s", a.httpConfig.Address())
+	log.Infof("🌐 Server is running on %s", app.httpConfig.Address())
 	log.Info("✅ Server started successfully")
-	if err := a.app.Listen(a.httpConfig.Address()); err != nil {
+	if err := app.app.Listen(app.httpConfig.Address()); err != nil {
 		return fmt.Errorf("✖ Failed to start http server: %s", err.Error())
 	}
 
 	return nil
 }
 
-func (a *App) initRouter() error {
-	api := a.app.Group("/api")
+func (app *App) initRouter() error {
+	api := app.app.Group("/api")
 
-	a.moduleProvider.authModule.AuthRoutes(api)
-	a.moduleProvider.userModule.UserRoutes(api)
+	app.moduleProvider.authModule.AuthRoutes(api)
+	app.moduleProvider.userModule.UserRoutes(api)
 	return nil
 }
--- backend/module/auth/service/setter.service.go
+++ backend/module/auth/service/setter.service.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	auth_dto "github.com/root9464/Ton-students/module/auth/dto"
-	user_dto "github.com/root9464/Ton-students/module/user/dto"
 	initdata "github.com/telegram-mini-apps/init-data-golang"
 )
 
