00d230e75707278f66e5434e54b08db90f54b26a
io <senpai9464@yandex.ru>
2024-12-21 17:15:00 +0000

    useless: -

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,12 @@
 import { useBackButton } from '@/shared/hooks/useBackButton';
 import { Button } from '@nextui-org/button';
 import { Input } from '@nextui-org/input';
+import { useLaunchParams } from '@telegram-apps/sdk-react';
 
 export default function Home() {
+  const { initDataRaw } = useLaunchParams();
+  console.log(initDataRaw);
+
   useBackButton(false);
 
   return (
